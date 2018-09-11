b12fba457923eeeb17bfa6a98c537f8453d52ad6
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-09-10 21:49:24 -0400

    add frog jump

---
+++ Lessons.Tests/Lesson3Tests.cs
@@ -0,0 +1,20 @@
+﻿using Lessons.Lesson3;
+using NUnit.Framework;
+
+namespace Lessons.Tests
+{
+    [TestFixture]
+    public class Lesson3Tests
+    {
+        [TestCase]
+        public void FrogJmp()
+        {
+            var instance = new FrogJmp();
+
+            Assert.AreEqual(3, instance.solution(10, 85, 30));
+            Assert.AreEqual(1, instance.solution(10, 40, 30));
+            Assert.AreEqual(2, instance.solution(10, 70, 30));
+            Assert.AreEqual(3, instance.solution(10, 71, 30));
+        }
+    }
+}
---
+++ Lessons/Lesson3/FrogJmp.cs
@@ -0,0 +1,17 @@
+﻿namespace Lessons.Lesson3
+{
+    public class FrogJmp
+    {
+        public int solution(int X, int Y, int D)
+        {
+            var diff = Y - X;
+
+            if (diff % D == 0)
+            {
+                return diff / D;
+            }
+
+            return diff / D + 1;
+        }
+    }
+}
