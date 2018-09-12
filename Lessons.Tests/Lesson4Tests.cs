54cce422a1f5c362b97748050b15caaacc33b107
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-09-11 22:06:37 -0400

    add permuation check

---
+++ Lessons.Tests/Lesson4Tests.cs
@@ -0,0 +1,18 @@
+﻿using Lessons.Lesson4;
+using NUnit.Framework;
+
+namespace Lessons.Tests
+{
+    [TestFixture]
+    public class Lesson4Tests
+    {
+        [TestCase]
+        public void PermCheck()
+        {
+            var instance = new PermCheck();
+
+            Assert.AreEqual(1, instance.solution(new[] { 2, 3, 1 }));
+            Assert.AreEqual(0, instance.solution(new[] { 2, 4, 1 }));
+        }
+    }
+}
---
+++ Lessons/Lesson4/PermCheck.cs
@@ -0,0 +1,22 @@
+﻿namespace Lessons.Lesson4
+{
+    public class PermCheck
+    {
+        public int solution(int[] A)
+        {
+            var found = new bool[A.Length];
+
+            foreach (var num in A)
+            {
+                if (num < 1 || num > A.Length || found[num - 1])
+                {
+                    return 0;
+                }
+
+                found[num - 1] = true;
+            }
+
+            return 1;
+        }
+    }
+}
