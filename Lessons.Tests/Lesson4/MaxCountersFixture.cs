17df3ab8dfa4771cefaa4f89fc391374f75c8d54
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-09-17 15:42:31 -0400

    add MaxCounters solution

---
+++ Lessons.Tests/Lesson4/MaxCountersFixture.cs
@@ -0,0 +1,20 @@
+﻿using Lessons.Lesson4;
+using NUnit.Framework;
+
+namespace Lessons.Tests.Lesson4
+{
+    [TestFixture]
+    public class MaxCountersFixture
+    {
+        [TestCaseSource(nameof(Data))]
+        public void Solution(int[] expected, int N, int[] A)
+        {
+            Assert.AreEqual(expected, new MaxCounters().Solution(N, A));
+        }
+
+        private static object[] Data => new object[]
+        {
+            new object[] { new[] { 3, 2, 2, 4, 2 }, 5, new[] { 3, 4, 4, 6, 1, 4, 4 } }
+        };
+    }
+}
---
+++ Lessons/Lesson4/MaxCounters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lessons.Lesson4
+{
+    public class MaxCounters
+    {
+        public int[] Solution(int N, int[] A)
+        {
+            var result = new int[N];
+            var max = 0;
+            var increase = 0;
+
+            foreach (var x in A)
+            {
+                if (x > N)
+                {
+                    increase = max;
+                }
+                else
+                {
+                    result[x - 1] = Math.Max(result[x - 1], increase);
+                    result[x - 1]++;
+                    max = Math.Max(max, result[x - 1]);
+                }
+            }
+
+            for (var i = 0; i < N; i++)
+            {
+                result[i] = Math.Max(result[i], increase);
+            }
+
+            return result;
+        }
+    }
+}
