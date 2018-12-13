2b014b46148144bc667c445190a3e06a3ee99bf1
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-12-12 20:50:18 -0500

    add solution for passing cars

---
+++ src/Lessons.Tests/Lesson5/PassingCarsFixture.cs
@@ -0,0 +1,25 @@
+﻿using Lessons.Lesson5;
+using NUnit.Framework;
+
+namespace Lessons.Tests.Lesson5
+{
+    class PassingCarsFixture
+    {
+        [TestCaseSource(nameof(Data))]
+        public void PassingCars(int expected, int[] a)
+        {
+            Assert.AreEqual(expected, new PassingCars().solution(a));
+        }
+
+        private static object[] Data => new object[]
+        {
+            new object[] {5, new[] { 0, 1, 0, 1, 1 } },
+            new object[] {0, new[] { 1 } },
+            new object[] {0, new[] { 1, 0 } },
+            new object[] {1, new[] { 0, 1 } },
+            new object[] {0, new[] { 1, 1 } },
+            new object[] {0, new[] { 0, 0 } },
+            new object[] {2, new[] { 0, 1, 1 } }
+        };
+    }
+}
---
+++ src/Lessons/Lesson5/PassingCars.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace Lessons.Lesson5
+{
+    public class PassingCars
+    {
+        private const int EAST = 0;
+        private const int WEST = 1;
+
+        public int solution(int[] A)
+        {
+            var total = 0;
+            var count = 0;
+
+            foreach (var direction in A)
+            {
+                if (direction == EAST)
+                {
+                    count++;
+                }
+
+                if (count > 0 && direction == WEST)
+                {
+                    total += count;
+                    if (total > 1000000000)
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return total;
+        }
+    }
+}
