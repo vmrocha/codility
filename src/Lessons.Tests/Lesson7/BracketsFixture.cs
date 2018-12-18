b38b5676cb4604d0c637e6112d98897a744b04d3
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-12-18 13:20:32 -0500

    add solution for Brackets exercise

---
+++ src/Lessons.Tests/Lesson7/BracketsFixture.cs
@@ -0,0 +1,26 @@
+﻿using Lessons.Lesson7.Brackets;
+using NUnit.Framework;
+
+namespace Lessons.Tests.Lesson7
+{
+    class BracketsFixture
+    {
+        [TestCaseSource(nameof(Data))]
+        public void Solution(int expected, string s)
+        {
+            var instance = new Solution();
+            Assert.AreEqual(expected, instance.solution(s));
+        }
+
+        private static object[] Data => new object[]
+        {
+            new object[] { 1, "{[()()]}" },
+            new object[] { 0, "([)()]" },
+            new object[] { 1, "(U)" },
+            new object[] { 1, "{U}" },
+            new object[] { 1, "[U]" },
+            new object[] { 0, "{[U]" },
+            new object[] { 0, "[U]}" }
+        };
+    }
+}
---
+++ src/Lessons/Lesson7/Brackets.cs
@@ -0,0 +1,40 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace Lessons.Lesson7.Brackets
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public int solution(string S)
+        {
+            var opens = new Stack<char>();
+
+            foreach (var c in S)
+            {
+                if (c == '{' || c == '[' || c == '(')
+                {
+                    opens.Push(c);
+                }
+                else if (c == '}' || c == ']' || c == ')')
+                {
+                    if (opens.Count == 0)
+                    {
+                        return 0;
+                    }
+
+                    var open = opens.Pop();
+                    if (open != '{' && c == '}' ||
+                        open != '(' && c == ')' ||
+                        open != '[' && c == ']')
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            return opens.Count == 0 ? 1 : 0;
+        }
+    }
+}
