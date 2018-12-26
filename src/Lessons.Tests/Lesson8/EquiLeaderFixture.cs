cc7a97b3ff0a0d67af3c5f33e72de9ac60e991c2
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-12-26 09:47:57 -0500

    add EquiLeader exercise solution

---
+++ src/Lessons.Tests/Lesson8/EquiLeaderFixture.cs
@@ -0,0 +1,20 @@
+﻿using Lessons.Lesson8.EquiLeader;
+using NUnit.Framework;
+
+namespace Lessons.Tests.Lesson8
+{
+    public class EquiLeaderFixture
+    {
+        [TestCaseSource(nameof(Data))]
+        public void Solution(int expected, int[] a)
+        {
+            var instance = new Solution();
+            Assert.AreEqual(expected, instance.solution(a));
+        }
+
+        private static object[] Data => new object[]
+        {
+            new object[] { 2, new[] { 4, 3, 4, 4, 4, 2 } }
+        };
+    }
+}
---
+++ src/Lessons/Lesson8/EquiLeader.cs
@@ -0,0 +1,63 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace Lessons.Lesson8.EquiLeader
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            var size   = 0;
+            var value  = 0;
+            var count  = 0;
+            var total  = 0;
+            var result = 0;
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                if (size == 0)
+                {
+                    size++;
+                    value = A[i];
+                }
+                else
+                {
+                    if (value != A[i])
+                    {
+                        size--;
+                    }
+                    else
+                    {
+                        size++;
+                    }
+                }
+            }
+
+            if (size > 0)
+            {
+                for (var i = 0; i < A.Length; i++)
+                {
+                    if (value == A[i])
+                    {
+                        total++;
+                    }
+                }
+
+                for (var i = 0; i < A.Length; i++)
+                {
+                    if (value == A[i])
+                    {
+                        count++;
+                    }
+
+                    if (count > (i + 1) / 2 && (total - count) > (A.Length - (i + 1)) / 2)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
